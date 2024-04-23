dea6b74fff30b71469df1f079b3eb5e1d950ea26
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-04-23 08:04:18 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -44,7 +44,7 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
         url = "https://github.com/atiiif/Force-Subs-Telegram-bot/issues/new"
         button = [
-            [InlineKeyboardButton(text = 'Admin Support', url="https://t.me/oftenes")],
+            [InlineKeyboardButton(text = 'Admin Support', url="https://t.me/ifeelscam")],
             [InlineKeyboardButton(text = 'Feature Request & Issues', url=url)],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
         ]
